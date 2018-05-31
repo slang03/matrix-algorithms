63e60ce0e066a323041bcf7110dd18e4650ff84c
Steven Lang <steven.lang.mz@gmail.com>
2018-06-01 11:16:28 +1200

    Add get [i,j] operator overload

--- src/main/java/com/github/waikatodatamining/matrix/core/Extensions.kt
+++ src/main/java/com/github/waikatodatamining/matrix/core/Extensions.kt
@@ -7,10 +7,17 @@ enum class MatrixEntryType {
     COL
 }
 
+/**
+ * Implement bracket accessor: matrix[j] = other
+ */
 operator fun Matrix.set(column: Int, vector: Matrix) {
     MatrixHelper.setColumnVector(vector, this, column)
 }
 
+
+/**
+ * Implement bracket accessor: matrix[i,j] = number
+ */
 operator fun Matrix.set(type: MatrixEntryType, idx: Int, vector: Matrix) {
     when (type) {
         MatrixEntryType.ROW -> MatrixHelper.setRowVector(vector, this, idx)
@@ -18,25 +25,44 @@ operator fun Matrix.set(type: MatrixEntryType, idx: Int, vector: Matrix) {
     }
 }
 
+/**
+ * Implement bracket accessor: matrix[i,j] = number
+ */
+operator fun Matrix.get(type: MatrixEntryType, idx: Int) : Matrix = when (type) {
+    MatrixEntryType.ROW -> MatrixHelper.rowAsVector(this, idx)
+    MatrixEntryType.COL -> MatrixHelper.columnAsVector(this, idx)
+}
+
+/**
+ * Add div opartor: matrix / double
+ */
 operator fun Matrix.div(value: Double): Matrix = this * (1.0 / value)
 
+/**
+ * Get this vector as a normalized vector
+ */
 fun Matrix.normalized(): Matrix {
     val copy = this.copy()
     MatrixHelper.normalizeVector(copy)
     return copy
 }
 
-fun Matrix.asDouble() = this[0, 0]
-
-
+/**
+ * Get l2 norm
+ */
 fun Matrix.l2() = MatrixHelper.l2VectorNorm(this)
 
+/**
+ * Get l2-norm squared
+ */
 fun Matrix.l2Sq(): Double {
     val l2 = this.l2()
     return l2 * l2
 }
 
-
+/**
+ * Add transposition as member
+ */
 val Matrix.T: Matrix
     get() = this.transpose()
 
